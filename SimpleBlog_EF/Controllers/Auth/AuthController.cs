4ceef9fda687829abd40d08cd1037cf60df43ccd
Kepler186f\beren <bezz.pezz@hotmail.com>
2017-01-31 16:56:22 +1000

    Added Basic Login Form and logic

--- SimpleBlog_EF/Controllers/Auth/AuthController.cs
+++ SimpleBlog_EF/Controllers/Auth/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog_EF.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,21 @@ namespace SimpleBlog_EF.Controllers.Auth
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+
+            if (form.Username != "beren" || form.Password != "pass")
+            {
+                ModelState.AddModelError("Username", "Username or Password isn't Valid");
+                return View(form);
+            }
+
+            return Content("The Form is Valid");
         }
     }
 }
