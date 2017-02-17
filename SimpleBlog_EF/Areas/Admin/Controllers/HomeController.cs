c3c8db7b0b8a9fb016ba9ada4e15f2acd40e910a
Kepler186f\beren <bezz.pezz@hotmail.com>
2017-02-17 13:55:26 +1000

    Implemented Index Action on post Admin

--- SimpleBlog_EF/Areas/Admin/Controllers/HomeController.cs
+++ SimpleBlog_EF/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using SimpleBlog_EF.Infrastructure;
+﻿using SimpleBlog_EF.Areas.Admin.ViewModels;
+using SimpleBlog_EF.DataAccessLayer;
+using SimpleBlog_EF.Infrastructure;
+using SimpleBlog_EF.Models.DataBase;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +11,34 @@ using System.Web.Mvc;
 namespace SimpleBlog_EF.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
+    [SelectedTab("home")]
     public class HomeController : Controller
     {
+        private MainDBContext db;
+        private const int postsPerPage = 3;
+
         // GET: Admin/Home
-        [SelectedTab("home")]
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-            return View();
+            var totalPostsCount = 0;
+
+            using (db = new MainDBContext())
+            {
+                totalPostsCount = db.Posts.Count();
+                var currentPostPage = db.Posts
+                        .Include("User")
+                        .Include("Tags")
+                    .OrderByDescending(c => c.CreatedAt)
+                    .Skip((page - 1) * postsPerPage)
+                    .Take(postsPerPage)
+                    .ToList();
+
+                return View(new PostsIndex
+                {
+                    Posts = new PageData<Post>(currentPostPage, totalPostsCount, page, postsPerPage)
+
+                });
+            }
         }
     }
 }
---
+++ SimpleBlog_EF/Areas/Admin/ViewModels/Posts.cs
@@ -0,0 +1,15 @@
+﻿using SimpleBlog_EF.Infrastructure;
+using SimpleBlog_EF.Models.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBlog_EF.Areas.Admin.ViewModels
+{
+    public class PostsIndex
+    {
+        public PageData<Post> Posts { get; set; }
+    }
+}
